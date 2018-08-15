6ea585c867bef8d1e8ff87f05654a081e721701e
ktavadze <ktavadze@gmail.com>
2018-08-15 17:25:19 -0400

    Refractors xml files

--- app/src/main/java/edu/ramapo/ktavadze/unipal/EventsRecyclerAdapter.java
+++ app/src/main/java/edu/ramapo/ktavadze/unipal/EventsRecyclerAdapter.java
@@ -2,12 +2,12 @@ package edu.ramapo.ktavadze.unipal;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -54,7 +54,7 @@ public class EventsRecyclerAdapter extends RecyclerView.Adapter<EventsRecyclerAd
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        ConstraintLayout recycler_event;
+        LinearLayout recycler_event;
         TextView recycler_event_name;
         TextView recycler_event_uid;
 
