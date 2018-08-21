acad5c061f945a52c848f2e91c21ee66d0d71025
ktavadze <ktavadze@gmail.com>
2018-08-20 22:05:22 -0400

    Adds basic styles to recycler events

--- app/src/main/java/edu/ramapo/ktavadze/unipal/EventsRecyclerAdapter.java
+++ app/src/main/java/edu/ramapo/ktavadze/unipal/EventsRecyclerAdapter.java
@@ -3,12 +3,12 @@ package edu.ramapo.ktavadze.unipal;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -33,7 +33,7 @@ public class EventsRecyclerAdapter extends RecyclerView.Adapter<EventsRecyclerAd
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        FrameLayout recycler_event;
+        CardView recycler_event;
         RelativeLayout recycler_event_background;
         LinearLayout recycler_event_foreground;
         TextView recycler_event_name;
