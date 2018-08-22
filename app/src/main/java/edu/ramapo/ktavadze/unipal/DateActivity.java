9f0a6701b6c03b634541baf39efe1604e98c3a29
ktavadze <ktavadze@gmail.com>
2018-08-22 01:20:58 -0400

    Cleans up date listeners

--- app/src/main/java/edu/ramapo/ktavadze/unipal/DateActivity.java
+++ app/src/main/java/edu/ramapo/ktavadze/unipal/DateActivity.java
@@ -1,9 +1,9 @@
 package edu.ramapo.ktavadze.unipal;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -17,14 +17,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.ArrayList;
 
 public class DateActivity extends AppCompatActivity {
-
     private static final String TAG = "DateActivity";
 
     private String mDate;
 
-    private DatabaseReference mEventData;
+    private DatabaseReference mEventsData;
+    private ChildEventListener mEventsListener;
 
     private ArrayList<Event> mEvents;
+    private EventsRecyclerAdapter mEventsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,21 +36,21 @@ public class DateActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(mDate);
+
+        addEventsListener();
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onDestroy() {
+        super.onDestroy();
 
-        getEventData();
+        removeEventsListener();
     }
 
     public void getIntentData() {
         final Intent intent = getIntent();
         if (intent.hasExtra("date")) {
             mDate = intent.getStringExtra("date");
-
-            mEventData = FirebaseDatabase.getInstance().getReference().child("events").child(User.getUid());
 
             Log.d(TAG, "getIntentData: Intent accepted");
         }
@@ -60,22 +61,26 @@ public class DateActivity extends AppCompatActivity {
         }
     }
 
-    public void getEventData() {
+    public void addEventsListener() {
+        // Init events
         mEvents = new ArrayList<>();
-        final EventsRecyclerAdapter eventsAdapter = new EventsRecyclerAdapter(this, mEvents);
+        mEventsAdapter = new EventsRecyclerAdapter(this, mEvents);
         final RecyclerView events_recycler = findViewById(R.id.date_events_recycler);
-        events_recycler.setAdapter(eventsAdapter);
+        events_recycler.setAdapter(mEventsAdapter);
         events_recycler.setLayoutManager(new LinearLayoutManager(this));
-        mEventData.addChildEventListener(new ChildEventListener() {
+
+        // Add events listener
+        mEventsData = FirebaseDatabase.getInstance().getReference().child("events").child(User.getUid());
+        mEventsListener = mEventsData.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Event event = dataSnapshot.getValue(Event.class);
                 if (event.getDate().equals(mDate)) {
                     mEvents.add(event);
 
-                    eventsAdapter.notifyDataSetChanged();
+                    mEventsAdapter.notifyDataSetChanged();
 
-                    Log.d(TAG, "onChildAdded: Event read");
+                    Log.d(TAG, "onChildAdded: Event read: " + event.getName());
                 }
             }
 
@@ -86,7 +91,14 @@ public class DateActivity extends AppCompatActivity {
 
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
+                Event event = dataSnapshot.getValue(Event.class);
+                if (event.getDate().equals(mDate)) {
+                    mEvents.remove(event);
 
+                    mEventsAdapter.notifyDataSetChanged();
+
+                    Log.d(TAG, "onChildRemoved: Event removed: " + event.getName());
+                }
             }
 
             @Override
@@ -99,5 +111,14 @@ public class DateActivity extends AppCompatActivity {
                 Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
             }
         });
+
+        Log.d(TAG, "addEventsListener: Listener added");
+    }
+
+    public void removeEventsListener() {
+        // Remove events listener
+        mEventsData.removeEventListener(mEventsListener);
+
+        Log.d(TAG, "removeEventsListener: Listener removed");
     }
 }
