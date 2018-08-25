b6c156f84c5094505202ff351a995070b3dd903c
ktavadze <ktavadze@gmail.com>
2018-08-25 19:08:58 -0400

    Adds basic styles to recycler schools

--- app/src/main/java/edu/ramapo/ktavadze/unipal/SchoolsRecyclerAdapter.java
+++ app/src/main/java/edu/ramapo/ktavadze/unipal/SchoolsRecyclerAdapter.java
@@ -3,12 +3,12 @@ package edu.ramapo.ktavadze.unipal;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -25,7 +25,7 @@ public class SchoolsRecyclerAdapter extends RecyclerView.Adapter<SchoolsRecycler
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        LinearLayout recycler_school;
+        CardView recycler_school;
         TextView recycler_school_name;
         TextView recycler_school_year;
         TextView recycler_school_major;
