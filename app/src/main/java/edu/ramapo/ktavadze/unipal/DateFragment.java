87a4b6d7dac3017fe9f5e4574b333afd91a78b0a
ktavadze <ktavadze@gmail.com>
2018-09-07 18:16:57 -0400

    Fixes new event creation from date view

--- app/src/main/java/edu/ramapo/ktavadze/unipal/DateFragment.java
+++ app/src/main/java/edu/ramapo/ktavadze/unipal/DateFragment.java
@@ -165,6 +165,7 @@ public class DateFragment extends Fragment implements RecyclerItemTouchHelper.Re
         final Spinner event_course_spinner = dialogView.findViewById(R.id.event_course_spinner);
         final TextView event_date_pick = dialogView.findViewById(R.id.event_date_pick);
         event_date_pick.setVisibility(View.GONE);
+        final Spinner event_alarm_spinner = dialogView.findViewById(R.id.event_alarm_spinner);
         final TextView event_time_pick = dialogView.findViewById(R.id.event_time_pick);
 
         // Type
@@ -213,6 +214,24 @@ public class DateFragment extends Fragment implements RecyclerItemTouchHelper.Re
                 }
             });
         }
+
+        // Alarm
+        event_alarm_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String alarm = parent.getItemAtPosition(position).toString();
+
+                // Set alarm
+                newEvent.setAlarm(alarm);
+
+                Log.d(TAG, "onItemSelected: Alarm selected: " + alarm);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         // Time
         event_time_pick.setOnClickListener(new View.OnClickListener() {
